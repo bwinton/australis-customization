fca6d77122c956fc57af4427b519cb76b05e370b
Blake Winton <bwinton@latte.ca>
2012-08-21 16:00:42 -0400

    Make some final fixes to the addon.

--- addon/lib/main.js
+++ addon/lib/main.js
@@ -12,7 +12,7 @@ case 'WINNT':
   OS = 'windows';
   break;
 case 'Darwin':
-  OS = 'mac'
+  OS = 'mac';
   break;
 }
 const customUrl = encodeURI(prefs.prototypeUrl.replace('%OS%', OS));
@@ -20,7 +20,7 @@ const customUrl = encodeURI(prefs.prototypeUrl.replace('%OS%', OS));
 var widget = widgets.Widget({
   id: "mozilla-link",
   label: "Mozilla website",
-  contentURL: "http://people.mozilla.com/~bwinton/australis-customization/images-win7/toolbarButton-customize.png",
+  contentURL: "http://people.mozilla.com/~bwinton/australis/customization/mac/images/toolbarButton-menu.png",
   onClick: function() {
     // Remove the location bar from the page.
     var whitelist = customUrl.toLowerCase();
@@ -39,6 +39,6 @@ var aboutHomeSearch = pageMod.PageMod({
   contentScriptWhen: 'ready',
   contentScript: 'document.addEventListener("tpemit", function(e) {self.port.emit("tp", e); return true;});',
   onAttach: function(worker) {
-    worker.port.on('tp', function(data) {console.log(data)});
+    worker.port.on('tp', function(data) {console.log(data);});
   }
 });
